daa1142e6b026bc64353029d7a57d0362bef6e50
Chittaranjan Swaminathan <ksatyaki@gmail.com>
2024-07-25 22:01:58 +0200

    Works. Need to make more robust

--- include/nav2_mod_planner/mod_planner.hpp
+++ include/nav2_mod_planner/mod_planner.hpp
@@ -3,104 +3,110 @@
 //
 #pragma once
 
-#include <memory>
-
-#include <geometry_msgs/msg/point.hpp>
-#include <nav2_core/global_planner.hpp>
-#include <nav2_costmap_2d/footprint_collision_checker.hpp>
-#include <nav_msgs/msg/path.hpp>
-
 #include <ompl/base/OptimizationObjective.h>
 #include <ompl/base/objectives/PathLengthOptimizationObjective.h>
 #include <ompl/geometric/SimpleSetup.h>
+#include <ompl/geometric/planners/informedtrees/AITstar.h>
 #include <ompl/geometric/planners/prm/PRMstar.h>
 #include <ompl/geometric/planners/rrt/RRTstar.h>
-#include <ompl/geometric/planners/informedtrees/AITstar.h>
 #include <ompl/mod/objectives/DTCOptimizationObjective.h>
 #include <ompl/mod/objectives/IntensityMapOptimizationObjective.h>
 #include <ompl/mod/objectives/UpstreamCriterionOptimizationObjective.h>
 #include <ompl/mod/samplers/HybridSampler.h>
 
+#include <geometry_msgs/msg/point.hpp>
+#include <memory>
+#include <nav2_core/global_planner.hpp>
+#include <nav2_costmap_2d/footprint_collision_checker.hpp>
+#include <nav_msgs/msg/path.hpp>
+
 namespace nav2_mod_planner {
 
-    class MoDPlanner : public nav2_core::GlobalPlanner {
-    public:
-        MoDPlanner();
+class MoDPlanner : public nav2_core::GlobalPlanner {
+ public:
+  MoDPlanner();
 
-        ~MoDPlanner() override = default;
+  ~MoDPlanner() override = default;
 
-        void configure(rclcpp_lifecycle::LifecycleNode::SharedPtr parent,
-                       std::string name, std::shared_ptr<tf2_ros::Buffer> tf,
-                       std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros);
+  void configure(
+      const rclcpp_lifecycle::LifecycleNode::WeakPtr &parent, std::string name,
+      std::shared_ptr<tf2_ros::Buffer> tf,
+      std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
 
-        void cleanup() override;
+  void cleanup() override;
 
-        void activate() override;
+  void activate() override;
 
-        void deactivate() override;
+  void deactivate() override;
 
-        nav_msgs::msg::Path
-        createPlan(const geometry_msgs::msg::PoseStamped &start,
-                   const geometry_msgs::msg::PoseStamped &goal) override;
+  nav_msgs::msg::Path createPlan(
+      const geometry_msgs::msg::PoseStamped &start,
+      const geometry_msgs::msg::PoseStamped &goal) override;
 
-    private:
-        struct steering_params {
-            double turning_radius;
-            double max_vehicle_speed;
-        } steering_params_;
+ private:
+  struct steering_params {
+    double turning_radius;
+    double max_vehicle_speed;
+  } steering_params_;
 
-        struct planner_params {
-            std::string sampler_type;
-            std::string objective_type;
-            double weight_euclidean, weight_quaternion, weight_mod;
-            std::string cliffmap_filename, gmmtmap_filename, intensitymap_filename;
-            double sampling_bias;
-            double max_planning_time;
-            double path_resolution;
-        } planner_params_;
+  struct planner_params {
+    std::string sampler_type;
+    std::string objective_type;
+    double weight_euclidean, weight_quaternion, weight_mod;
+    std::string cliffmap_filename, gmmtmap_filename, intensitymap_filename;
+    double sampling_bias;
+    double max_planning_time;
+    double path_resolution;
+  } planner_params_;
 
-        // TF buffer
-        std::shared_ptr<tf2_ros::Buffer> tf_;
+  // TF buffer
+  std::shared_ptr<tf2_ros::Buffer> tf_;
 
-        // node ptr
-        nav2_util::LifecycleNode::SharedPtr node_;
+  // node ptr
+  nav2_util::LifecycleNode::SharedPtr node_;
 
-        // Global Costmap
-        nav2_costmap_2d::Costmap2D *costmap_;
+  // Global Costmap
+  nav2_costmap_2d::Costmap2D *costmap_;
 
-        // The global frame of the costmap
-        std::string global_frame_, name_;
+  // The global frame of the costmap
+  std::string global_frame_, name_;
 
-        // Motion plan costs topic
-        std::string motion_plan_costs_topic_;
+  // Motion plan costs topic
+  std::string motion_plan_costs_topic_;
 
-        // An OMPL simple setup ptr
-        ompl::geometric::SimpleSetupPtr simple_setup_;
+  // An OMPL simple setup ptr
+  ompl::geometric::SimpleSetupPtr simple_setup_;
 
-        // Optimization Objective ptr
-        std::shared_ptr<ompl::base::OptimizationObjective> optimization_objective_;
+  // Optimization Objective ptr
+  std::shared_ptr<ompl::base::OptimizationObjective> optimization_objective_;
 
-        class FootprintStateValidityChecker : public ompl::base::StateValidityChecker {
-        public:
-            FootprintStateValidityChecker(const ompl::base::SpaceInformationPtr &si,
-                                          nav2_costmap_2d::FootprintCollisionChecker<nav2_costmap_2d::Costmap2D *> *checker,
-                                          const std::vector<geometry_msgs::msg::Point> &footprint)
-                    : ompl::base::StateValidityChecker(si), checker_(checker), footprint_(footprint) {}
+  class FootprintStateValidityChecker
+      : public ompl::base::StateValidityChecker {
+   public:
+    FootprintStateValidityChecker(
+        const ompl::base::SpaceInformationPtr &si,
+        std::shared_ptr<nav2_costmap_2d::FootprintCollisionChecker<nav2_costmap_2d::Costmap2D *>>
+            checker,
+        const std::vector<geometry_msgs::msg::Point> &footprint)
+        : ompl::base::StateValidityChecker(si),
+          checker_(checker),
+          footprint_(footprint) {}
 
-            bool isValid(const ompl::base::State *state) const override {
-                return checker_->footprintCostAtPose(
-                        state->as<ompl::base::SE2StateSpace::StateType>()->getX(),
-                        state->as<ompl::base::SE2StateSpace::StateType>()->getY(),
-                        state->as<ompl::base::SE2StateSpace::StateType>()->getYaw(),
-                        footprint_) >= nav2_costmap_2d::LETHAL_OBSTACLE;
-            }
+    bool isValid(const ompl::base::State *state) const override {
+      return checker_->footprintCostAtPose(
+                 state->as<ompl::base::SE2StateSpace::StateType>()->getX(),
+                 state->as<ompl::base::SE2StateSpace::StateType>()->getY(),
+                 state->as<ompl::base::SE2StateSpace::StateType>()->getYaw(),
+                 footprint_) < static_cast<double>(nav2_costmap_2d::LETHAL_OBSTACLE);
+    }
 
-        private:
-            nav2_costmap_2d::FootprintCollisionChecker<nav2_costmap_2d::Costmap2D *> *checker_;
-            std::vector<geometry_msgs::msg::Point> footprint_;
-        };
+   private:
+    std::shared_ptr<nav2_costmap_2d::FootprintCollisionChecker<nav2_costmap_2d::Costmap2D *>>
+        checker_;
+    std::vector<geometry_msgs::msg::Point> footprint_;
+  };
 
-        std::shared_ptr<ompl::base::StateValidityChecker> state_validity_checker_;
-    };
+  std::shared_ptr<ompl::base::StateValidityChecker> state_validity_checker_;
+};
 
-} // namespace nav2_mod_planner
+}  // namespace nav2_mod_planner
